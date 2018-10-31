5f5a002de2fff19fc3f77fa7c619a27def02e6f2
Travis CI <travis@travis-ci.org>
2018-10-31 13:03:29 +0900

    Fix pull commands

--- pull.sh
+++ pull.sh
@@ -3,7 +3,9 @@
 mkdir _site
 cd _site
 
+git init
 git remote add origin https://${GH_TOKEN}:x-oauth-basic@github.com/ugwis/hp.git > /dev/null 2>&1
-git pull origin gh-pages
+git fetch
+git checkout gh-pages
 
 cd ../
